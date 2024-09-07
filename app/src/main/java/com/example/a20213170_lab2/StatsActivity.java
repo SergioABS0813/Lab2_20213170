3c11fd4b8d9ea03378ddfcedb9e8ed1993e01419
SergioABS0813 <sergioandre.bustamante@gmail.com>
2024-09-07 18:32:44 -0500

    soloFaltaMostrarHistorial

---
+++ app/src/main/java/com/example/a20213170_lab2/StatsActivity.java
@@ -0,0 +1,38 @@
+package com.example.a20213170_lab2;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StatsActivity extends AppCompatActivity {
+
+    private List<String> historialTiempos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_stats);
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+
+            historialTiempos = bundle.getStringArrayList("historial");
+
+        }
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
--- app/src/main/java/com/example/a20213170_lab2/TeleGame.java
+++ app/src/main/java/com/example/a20213170_lab2/TeleGame.java
@@ -1,10 +1,15 @@
 package com.example.a20213170_lab2;
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -20,6 +25,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TeleGame extends AppCompatActivity {
 
     private TextView[] caracterViews;
@@ -32,21 +40,22 @@ public class TeleGame extends AppCompatActivity {
     private int cantidadpartes = 6;
     private int actualParte;
     private Button newGame;
+    private long tiempoInicio;
+    private long tiempoFin;
+    //Lista para guardar los tiempos
+    private List<String> tiemposString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_tele_game);
-
         botonesLetras = findViewById(R.id.botonesLetras);
         gridView = findViewById(R.id.letters);
         newGame = findViewById(R.id.newGame);
-
+        tiempoInicio = System.currentTimeMillis();
+        tiemposString = new ArrayList<>();
         jugarGame();
-
-
-
         partesHumano = new ImageView[cantidadpartes];
         partesHumano[0] = findViewById(R.id.head);
         partesHumano[1] = findViewById(R.id.body);
@@ -109,17 +118,22 @@ public class TeleGame extends AppCompatActivity {
         if(correct){
             if(numCorr == numChar){
                 //Usuario ganó
+                tiempoFin = System.currentTimeMillis(); //Acabo y gano
+                long duracion = tiempoFin - tiempoInicio; //en milisegundos
+                System.out.println(duracion/1000);
+                tiemposString.add(String.valueOf(duracion/1000));
                 deshabilitarBotones();
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setTitle("Ganaste");
-                builder.setMessage("Felicidades!\n\n La respuesta era: \n\n " + palabra);
+                builder.setTitle("Ganaste :D");
+                builder.setMessage("Felicidades!\n\n La respuesta era: \n\n " + palabra +
+                        "\n\n Terminó en " + duracion/1000 + " segundos.");
                 builder.setPositiveButton("Jugar de nuevo", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        TeleGame.this.jugarGame();
+                        recreate();
                     }
                 });
-                builder.setPositiveButton("Salir", new DialogInterface.OnClickListener() {
+                builder.setNegativeButton("Salir", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         TeleGame.this.finish();
@@ -135,18 +149,23 @@ public class TeleGame extends AppCompatActivity {
 
         }else{
             partesHumano[actualParte].setVisibility(View.VISIBLE);
+            tiempoFin = System.currentTimeMillis(); //Acabo y perdió
+            long duracion = tiempoFin - tiempoInicio; //en milisegundos
+            System.out.println(duracion/1000); //calculo en segundos
+            tiemposString.add(String.valueOf(duracion/1000));
             deshabilitarBotones(); //Falta logica cuano actualParte = 6
             //Usuario ya perdió
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle("Perdiste");
-            builder.setMessage("La respuesta era: \n\n " + palabra);
+            builder.setTitle("Perdiste :(");
+            builder.setMessage("Sigue intentando!\n\n La respuesta era: \n\n " + palabra +
+                    "\n\n Terminó en " + duracion/1000 + " segundos.");
             builder.setPositiveButton("Jugar de nuevo", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
-                    TeleGame.this.jugarGame();
+                    recreate();
                 }
             });
-            builder.setPositiveButton("Salir", new DialogInterface.OnClickListener() {
+            builder.setNegativeButton("Salir", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
                     TeleGame.this.finish();
@@ -167,8 +186,30 @@ public class TeleGame extends AppCompatActivity {
 
     public void onClickNewGame(View view) {
         if(view.getId() == R.id.newGame){ //Cuando le damos clic a "Nuevo Juego"
-            recreate();
+            recreate(); //Refresca la actividad si le damos clic a Nuevo Juego
         }
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+
+        if(item.getItemId() == R.id.estadisticas){
+            Bundle historialbundle = new Bundle();
+            historialbundle.putStringArrayList("historial", (ArrayList<String>) tiemposString);
+            Intent intent = new Intent(this, StatsActivity.class);
+            intent.putExtras(historialbundle);
+            startActivity(intent);
+
+        }
+        return true;
+
+    }
+
 }
